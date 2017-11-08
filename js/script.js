775e92aaf587a34a4d73c280b23acb9b7524f421
Mitchel Harding <hardingmitchel@gmail.com>
2017-11-08 11:55:29 -0700

    SO ive copy and pasted Jquery script that mitigates the repition of selectors abd variables

--- js/script.js
+++ js/script.js
@@ -1,23 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $("#blanks form").submit(function() {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-  $("#formOne").submit(function(){
-     var person1Input = $("input#person1").val();
-     var person2Input = $("input#person2").val();
-     var animalInput = $("input#animal").val();
-     var exclamationInput = $("input#exclamation").val();
-     var verbInput = $("input#verb").val();
-     var nounInput = $("input#noun").val();
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      console.log(userInput)
+      $("." + blank).text(userInput).val();
+    });
 
-     $(".person1").text(person1Input);
-     $(".person2").text(person2Input);
-     $(".animal").text(animalInput);
-     $(".exclamation").text(exclamationInput);
-     $(".verb").text(verbInput);
-     $(".noun").text(nounInput);
+    $("#story").show();
 
-     $("#story").show();
-
-     event.preventDefault();
-
+    event.preventDefault();
   });
 });
